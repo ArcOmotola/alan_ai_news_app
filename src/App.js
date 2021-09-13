b14029d2241686fdfef58a411ef422a76f70bc3c
ArcOmotola <ogunsola40@gmail.com>
2021-09-13 18:20:54 +0100

    implemented alan API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = 'd59d94041d6988171e0249854279c8472e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 export default function App() {
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                }
+            }
+        })
+    }, []);
+
     return (
         <div>
             <h1>Alan AI News Application</h1>
