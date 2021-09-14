951f0822b6734289c439c33f3e4f9dc2988f3f86
ArcOmotola <ogunsola40@gmail.com>
2021-09-14 18:04:01 +0100

    implemented auto scroll-to-window  on read feature

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import classNames from 'classnames';
 import useStyles from './styles';
 
 export default function NewsCard({ article: {description, publishedAt, source, title, url, urlToImage}, i, activeArticle}) {
     const classes = useStyles();
-    
+    const [elRefs, setElRefs] = useState([]);
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if(i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs]);
+
     return (
-        <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+        <Card ref ={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
             <CardActionArea href={url} target="_blank">
                 <CardMedia className={classes.media} image={urlToImage || 'https://www.dzr.de/Sync/An%20ABZ/image-thumb__2148__website-image/news%21~-~575w.jpeg'}/>
                 <div className={classes.details}>
