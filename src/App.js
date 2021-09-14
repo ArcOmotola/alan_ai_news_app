1a2dec35aca5e02353c658ee7fd1e2f19421eee0
ArcOmotola <ogunsola40@gmail.com>
2021-09-14 21:14:23 +0100

    implemented card number response

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordToNumbers from 'word-to-numbers';
 import NewsCards from './components/NewsCards/NewsCards';
 import useStyles from './styles';
 
@@ -13,12 +14,24 @@ export default function App() {
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if(command === 'newHeadlines') {
                     setNewsArticles(articles);
                     setActiveArticle(-1);
                 } else if (command === 'highlight') {
                     setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+                } else if (command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordToNumbers(number, { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if(parsedNumber > 20) {
+                        alanBtn().playText('Please try that again.');
+                    } else if (article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    }
+
+                    
                 }
             }
         })
